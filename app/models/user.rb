b94b48e66d68288eeedfe7ca6fc87f7e76a1269e
btrihatmaja <astungkara.project@gmail.com>
2014-12-06 21:17:16 +0800

    secret key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
