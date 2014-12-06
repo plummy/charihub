045cd4bcbd889da51478e146cd25b521011d69fa
btrihatmaja <astungkara.project@gmail.com>
2014-12-06 21:33:58 +0800

    add subcategory model

---
+++ db/migrate/20141206133323_create_subcategories.rb
@@ -0,0 +1,10 @@
+class CreateSubcategories < ActiveRecord::Migration
+  def change
+    create_table :subcategories do |t|
+      t.string :name
+      t.text :desc
+
+      t.timestamps
+    end
+  end
+end
