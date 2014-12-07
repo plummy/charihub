eb7a70f49b8be1180338cf9c3f10053f86612e38
btrihatmaja <astungkara.project@gmail.com>
2014-12-07 23:39:40 +0800

    adding gem for chart, adding address to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207124333) do
+ActiveRecord::Schema.define(version: 20141207153852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141207124333) do
     t.boolean  "status",       default: false
     t.integer  "view_counter"
     t.integer  "type"
+    t.string   "address"
   end
 
   create_table "users", force: true do |t|
