f58731c8435b40944dd3581728137b0b6790f3f2
btrihatmaja <astungkara.project@gmail.com>
2014-12-07 20:42:11 +0800

    post model add enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ActiveRecord::Base
 	validates :name, presence: true
+	validates :type, presence: true
 	has_many :comments, dependent: :destroy
 	belongs_to :user
 	belongs_to :category
+
+	enum type: { 
+	  share: 0, 
+	  ask: 1}
 end
