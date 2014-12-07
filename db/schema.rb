67d023ad39875c127a9b8f8e375ae9ee45eb11f4
btrihatmaja <astungkara.project@gmail.com>
2014-12-07 23:56:22 +0800

    adding phone to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207153852) do
+ActiveRecord::Schema.define(version: 20141207155549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20141207153852) do
     t.integer  "view_counter"
     t.integer  "type"
     t.string   "address"
+    t.integer  "phone"
   end
 
   create_table "users", force: true do |t|
