4c3adeda5a217fcfaa64896406a683e224af3991
btrihatmaja <astungkara.project@gmail.com>
2014-12-09 14:16:53 +0800

    paperclip setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,10 @@ class User < ActiveRecord::Base
 
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
+
+	# Paperclip
+	has_attached_file :avatar, :styles => { :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage/
+	validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
 	
 end
