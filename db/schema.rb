97cfdfb0f499e079c8555e751e325bf087cd6ad5
btrihatmaja <astungkara.project@gmail.com>
2014-12-09 13:59:14 +0800

    migration paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207194029) do
+ActiveRecord::Schema.define(version: 20141209055706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20141207194029) do
     t.float    "longitude"
     t.float    "latitude"
     t.string   "full_address"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
